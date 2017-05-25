6485b2a4c4069344bfc3a6f518f63362242e6c90
Vladimir.asdevel <Vladimir.asdevel@gmail.com>
2017-05-25 16:40:15 +0400

    Тестирование кода в Playgroung

--- CalculatorPlayground.playground/Contents.swift
+++ CalculatorPlayground.playground/Contents.swift
@@ -7,7 +7,59 @@ var str = "Hello, playground"
 func changeSign(operand: Double) -> Double {
     return -operand
 }
+
 let i = 27
+
 var f: (Double) -> Double
+
+f = cos
+let `defer` = f(Double.pi)
+let defer2 = `defer` + 5
+
+print(`defer`, defer2)
+debugPrint(`defer`, defer2)
+
 f = changeSign
 let x = f(81)
+
+let decimalInt     = 17
+let octalInt       = 0o21
+let binaryInt      = 0b10001
+let hexadecimalInt = 0x11
+
+1.25e2
+1.25e-2
+
+0xFp2
+0xFp-2
+
+let decimalDouble     = 12.1875
+let exponentDouble    = 1.21875e1
+let hexadecimalDouble = 0xC.3p0
+
+let paddedDouble = 000123.456
+let oneMillion   = 1_000_000
+let justOverOneMillion = 1_000_000.000_000_1
+let badTwoMillions = 200_0000
+
+/// Ну что ж, попробуем сравнить кортежи с булевыми значениями 😎
+/// Cначала нормальные сравниваемые кортежи
+(1, "zebra") < (2, "apple")
+(3, "apple") < (4, "bird")
+(4, "dog") == (4, "dog")
+/// А теперь кортежи с булевыми значениями
+(true, 1) == (false, 1)
+(true, 2) == (true, 2)
+(false, 3) == (false, 3)
+(false, 4) == (false, 5)
+(true, 6) == (true, 7)
+
+//(true, 1) <= (false, 1)
+//(true, 2) >= (true, 2)
+//(false, 3) < (false, 3)
+(false, 4) != (false, 5)
+//(true, 6) > (true, 7)
+
+
+
+
